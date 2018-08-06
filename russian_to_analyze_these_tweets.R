7c3977f4cd59e25c62c299314131d14ff744e5b5
Matt Gilmore <mtgilmore@comcast.net>
2018-08-06 10:06:03 -0400

    Add histogram

--- russian_to_analyze_these_tweets.R
+++ russian_to_analyze_these_tweets.R
@@ -5,3 +5,16 @@ for (i in 2:9) {
   temp <- read.csv(string)
   tweets <- rbind(tweets, temp)
 }
+
+time_stamp <- tweets[['publish_date']]
+
+time_stamp <- as.POSIXct(time_stamp, format="%m/%d/%Y %H:%M", tz="GMT")
+
+hist(time_stamp, breaks="days", freq=TRUE)
+
+#right = data.frame()
+#for (i in 1:length(tweets[,1])) {
+#  if (tweets["account_type",i] = "Right") {
+#    append(right, tweets[i,])
+#  }
+#}
