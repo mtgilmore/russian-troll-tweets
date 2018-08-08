0eb3d7322fa1ddf9889ef0e4c3546388349635b8
Matt Gilmore <mtgilmore@comcast.net>
2018-08-08 14:34:25 -0400

    Add parse of left tweets

--- russian_to_analyze_these_tweets.R
+++ russian_to_analyze_these_tweets.R
@@ -16,8 +16,10 @@ hist(time_stamp, breaks="days", freq=TRUE)
 right <- setNames(data.frame(matrix(ncol = 15, nrow = 0)),
                   c("external_author_id", "author", "content", "region", "language", "publish_date", "harvested_date",
                     "following", "followers", "updates", "post_type", "account_type", "new_june_2018", "retweet", "account_category"))
-
-for (i in 1:50)
+left <- setNames(data.frame(matrix(ncol = 15, nrow = 0)),
+                 c("external_author_id", "author", "content", "region", "language", "publish_date", "harvested_date",
+                   "following", "followers", "updates", "post_type", "account_type", "new_june_2018", "retweet", "account_category"))
+for (i in 1:length(tweets[,1]))
 {
   
   if (tweets[i, 15] == "RightTroll")
@@ -25,5 +27,10 @@ for (i in 1:50)
     rowtemp <- tweets[i,]
     right <- rbind(right, rowtemp)
   }
-
+  if (tweets[i, 15] == "LeftTroll")
+  {
+    rowtemp <- tweets[i,]
+    left <- rbind(left, rowtemp)
+  }
+  
 }
